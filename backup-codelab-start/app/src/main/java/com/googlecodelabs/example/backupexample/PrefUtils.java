1071a03245e914c17ecaddbe08eee8bbd393372c
Manuel Vivo <mvivo@google.com>
2019-11-08 14:45:02 +0100

    Updates to AndroidX

--- backup-codelab-start/app/src/main/java/com/googlecodelabs/example/backupexample/PrefUtils.java
+++ backup-codelab-start/app/src/main/java/com/googlecodelabs/example/backupexample/PrefUtils.java
@@ -16,10 +16,9 @@ package com.googlecodelabs.example.backupexample;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.text.Html;
-import android.text.Spannable;
 import android.text.Spanned;
 
 /**
